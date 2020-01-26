6b87f90df40db29a8375c47d729af31bd2445cbe
Stefan Kummer <stefan.kummer@bluewin.ch>
2020-01-26 09:34:31 +0100

    Replacer bloginfo() par get_bloginfo()

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta charset="<?php get_bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
 
     <?php wp_head(); ?>

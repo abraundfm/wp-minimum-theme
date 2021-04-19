3db9f85d8a1efbff6cb5be7f0264e33a64f4c388
thebavariancoder <andreasbraun.bavaria@gmail.com>
2021-04-19 23:35:16 +0200

    init

--- header.php
+++ header.php
@@ -1,10 +1,14 @@
 <!DOCTYPE html>
+
 <html <?php language_attributes(); ?>>
+
 <head>
     <meta charset="<?php get_bloginfo('charset'); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
     <?php wp_head(); ?>
+    
 </head>
 
 <body <?php body_class(); ?>>
--- index.php
+++ index.php
@@ -1,13 +1,12 @@
-<!-- On appelle le header -->
 <?php get_header(); ?>
 
-<!-- C'est ici qu'on mettra le contenu du site, comme par exemple la boucle wordpress -->
-<?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-    <h1><?php the_title(); ?></h1>
-    <?php the_content(); ?>
+        <h1><?php the_title(); ?></h1>
 
-<?php endwhile; endif; ?>
+        <?php the_content(); ?>
 
-<!-- On appelle le footer -->
+<?php endwhile;
+endif; ?>
+
 <?php get_footer(); ?>
